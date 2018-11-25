e5b6bee65690a24e4f1254c6484370eb8df031b6
Erd <erd@conceptual.ca>
2018-11-25 13:26:56 -0500

    mkdir

--- install.lua
+++ install.lua
@@ -5,6 +5,10 @@ if args[1] then
     installDir = args[1];
 end
 
+if installDir ~= "" then
+    local fs = require("filesystem");
+    fs.makeDirectory(installDir);
+end
 
 shell.execute('rm -r ' .. installDir .. '/usr/lib/oop');
 shell.execute('cp -r ./usr/* ' .. installDir .. '/usr/');
